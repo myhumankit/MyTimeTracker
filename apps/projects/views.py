eb14df77396fa861303d99b1aca94b108ad1db9e
Julien Lebunetel <julien@lebunetel.com>
2019-09-23 11:23:56 +0200

    Limit Project and Leave view to request.user

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -13,7 +13,15 @@ class ActivityListView(LoginRequiredMixin, ListView):
     model = Activity
     paginate_by = 50
 
+    def get_queryset(self):
+        queryset = Activity.objects.filter(user=self.request.user)
+        return queryset
+
 
 class LeaveListView(LoginRequiredMixin, ListView):
 
     model = Leave
+
+    def get_queryset(self):
+        queryset = Leave.objects.filter(user=self.request.user)
+        return queryset
